47ada9e3cafe6fbaad56f44cdf7945285826db50
Manfred Touron <m@42.am>
2016-09-02 16:27:56 +0200

    Initial version of the API

--- number.go
+++ number.go
@@ -1,15 +1,38 @@
 package numberinfo
 
+import "strconv"
+
 type Number struct {
-	int64 int64
+	int64   int64
+	float64 float64
 }
 
-func Int64(input int64) Number {
-	return Number{
-		int64: input,
+func Int64(input int64) *Number {
+	return &Number{
+		int64:   input,
+		float64: float64(input),
+	}
+}
+
+func Float64(input float64) *Number {
+	return &Number{
+		int64:   int64(input),
+		float64: input,
 	}
 }
 
 func (n *Number) Int64() int64 {
 	return n.int64
+}
+
+func (n *Number) Int() int {
+	return int(n.int64)
+}
+
+func (n *Number) Float64() float64 {
+	return n.float64
+}
+
+func (n *Number) String() string {
+	return strconv.Itoa(n.Int())
 }
--- number_test.go
+++ number_test.go
@@ -8,7 +8,42 @@ import (
 
 func TestInt64(t *testing.T) {
 	Convey("Testing Int64()", t, func() {
-		number := Int64(4242)
-		So(number.Int64(), ShouldEqual, 4242)
+		So(Int64(4242).int64, ShouldEqual, 4242)
+		So(Int64(-4242).int64, ShouldEqual, -4242)
+	})
+}
+
+func TestFloat64(t *testing.T) {
+	Convey("Testing Float64()", t, func() {
+		So(Float64(4242.0).float64, ShouldEqual, 4242.0)
+		So(Float64(-4242.0).float64, ShouldEqual, -4242.0)
+	})
+}
+
+func TestNumber_Int64(t *testing.T) {
+	Convey("Testing Number.Int64()", t, func() {
+		So(Int64(4242).Int64(), ShouldEqual, 4242)
+		So(Int64(-4242).Int64(), ShouldEqual, -4242)
+	})
+}
+
+func TestNumber_Float64(t *testing.T) {
+	Convey("Testing Number.Float64()", t, func() {
+		So(Int64(4242).Float64(), ShouldEqual, 4242)
+		So(Int64(-4242).Float64(), ShouldEqual, -4242)
+	})
+}
+
+func TestNumber_Int(t *testing.T) {
+	Convey("Testing Number.Int()", t, func() {
+		So(Int64(4242).Int(), ShouldEqual, 4242)
+		So(Int64(-4242).Int(), ShouldEqual, -4242)
+	})
+}
+
+func TestNumber_String(t *testing.T) {
+	Convey("Testing Number.String()", t, func() {
+		So(Int64(4242).String(), ShouldEqual, "4242")
+		So(Int64(-4242).String(), ShouldEqual, "-4242")
 	})
 }
